7ee2445fee2c05c37b930a1699dd1f60a4303db9
Jason Yeomans <jyeo098@gmail.com>
2013-11-13 01:06:48 -0500

    Mechanics Refinery
    Refined a plethora of current player mechanics (added gravity!)

--- src/com/dejavu/tdarcade/BlockMap.java
+++ src/com/dejavu/tdarcade/BlockMap.java
@@ -5,21 +5,33 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 import org.newdawn.slick.util.pathfinding.PathFindingContext;
 import org.newdawn.slick.util.pathfinding.TileBasedMap;
- 
+
 public class BlockMap implements TileBasedMap {
 	public static TiledMap tmap;
 	public static int mapWidth;
 	public static int mapHeight;
+	public static int mapSize;
 	public static int blockingLayerId = 0;
+	private int square[] = { 1, 1, 15, 1, 15, 15, 1, 15 };
 	public static ArrayList<Object> entities;
- 
+
 	public BlockMap(String ref) throws SlickException {
 		entities = new ArrayList<Object>();
 		tmap = new TiledMap(ref, "data/levels");
 		mapWidth = tmap.getWidth() * tmap.getTileWidth();
 		mapHeight = tmap.getHeight() * tmap.getTileHeight();
+		mapSize = mapWidth * mapHeight;
+
+		for (int x = 0; x < tmap.getWidth(); x++) {
+			for (int y = 0; y < tmap.getHeight(); y++) {
+				int tileID = tmap.getTileId(x, y, 0);
+				if (tileID == 1) {
+					entities.add(new Block(x * 15, y * 15, square, "sqaure"));
+				}
+			}
+		}
 	}
-	
+
 	@Override
 	public boolean blocked(PathFindingContext ctx, int x, int y) {
 		int tileID = tmap.getTileId(x, y, 0);
@@ -43,11 +55,11 @@ public class BlockMap implements TileBasedMap {
 
 	@Override
 	public void pathFinderVisited(int arg0, int arg1) {
-		
+
 	}
 
 	public int getTileId(int x, int y, int i) {
 		return tmap.getTileId(x, y, i);
-		
+
 	}
 }
--- src/com/dejavu/tdarcade/Player.java
+++ src/com/dejavu/tdarcade/Player.java
@@ -10,22 +10,22 @@ import org.newdawn.slick.geom.Polygon;
 
 public class Player {
 
-	private int curX, curY;
+	private float curX, curY, jumpCount, g, v;
 	private boolean facingRight;
 	private Polygon playerPoly;
 	private Input input;
+	private Block entity;
 
 	// SpriteSheets
-	private SpriteSheet walkingSheet;
-	private SpriteSheet idleSheet;
+	private SpriteSheet walkingSheet, walkingLeftSheet, idleSheet;
 	private final int[] numFrames = { 1, 4, 0, 0 };
 
 	// Animations
-	private Animation walkingAnimation;
-	private Animation idleAnimation;
+	private Animation walkingAnimation, walkingLeftAnimation, idleAnimation;
 
 	// Actions
 	private int action;
+	private boolean jumping;
 	private static final int IDLE = 0;
 	private static final int WALKINGR = 1;
 	private static final int WALKINGL = 2;
@@ -34,15 +34,20 @@ public class Player {
 
 	public Player(GameContainer gc) throws SlickException {
 		facingRight = true;
-		curX = 120;
-		curY = 150;
+		curX = 70;
+		curY = 120;
 		input = gc.getInput();
 		action = IDLE;
+		jumping = false;
+		g = 0.25f;
+		v = 2;
 
 		// Load SpriteSheets
 		try {
 			walkingSheet = new SpriteSheet(
 					"data/spritesheets/walking_spritesheet.png", 80, 150);
+			walkingLeftSheet = new SpriteSheet(
+					"data/spritesheets/walkingLeft_spritesheet.png", 80, 150);
 			idleSheet = new SpriteSheet(
 					"data/spritesheets/idle_spritesheet.png", 80, 150);
 		} catch (Exception e) {
@@ -50,6 +55,7 @@ public class Player {
 		}
 
 		walkingAnimation = new Animation(walkingSheet, 200);
+		walkingLeftAnimation = new Animation(walkingLeftSheet, 200);
 		idleAnimation = new Animation(idleSheet, 100);
 
 		playerPoly = new Polygon(new float[] { curX, curY, curX + 80, curY,
@@ -57,44 +63,99 @@ public class Player {
 	}
 
 	public void update(GameContainer gc, int i) throws SlickException {
-		// TODO Auto-generated method stub
-		if (input.isKeyDown(Input.KEY_D)) {
-			moveX(2);
-			walkingAnimation.update(i);
-			action = WALKINGR;
-		} else if (input.isKeyDown(Input.KEY_A)) {
-			moveX(-2);
-			walkingAnimation.update(i);
-			action = WALKINGR;
+		if (jumping && jumpCount <= 30) {
+			moveY(-v);
+			playerPoly.setY(curY);
+			idleAnimation.update(i);
+			if (entityCollisionWith()) {
+				curY += v;
+				playerPoly.setY(curY);
+				v = 2;
+				jumping = false;
+			}
+			jumpCount++;
+			v -= g;
 		} else {
+			jumping = false;
+			curY += v;
+			playerPoly.setY(curY);
+			if (entityCollisionWith()) {
+				curY -= v;
+				playerPoly.setY(curY);
+				v = 2;
+			}
+			v += g;
+		}
+
+		if (input.isKeyDown(Input.KEY_SPACE) && !jumping) {
+			jumpCount = 0;
+			jumping = true;
+			v = 8;
+		} else if (input.isKeyDown(Input.KEY_D)) {
+			moveX(3);
+			playerPoly.setX(curX);
+			walkingAnimation.update(i);
+			action = WALKINGR;
+			if (entityCollisionWith()) {
+				moveX(-3);
+				playerPoly.setX(curX);
+			}
+		} else if (input.isKeyDown(Input.KEY_A)) {
+			moveX(-3);
+			playerPoly.setX(curX);
+			walkingLeftAnimation.update(i);
+			action = WALKINGL;
+			if (entityCollisionWith()) {
+				moveX(3);
+				playerPoly.setX(curX);
+			}
+
+		} else if (!jumping) {
 			idleAnimation.update(i);
 			action = IDLE;
 		}
 	}
 
-	public void moveX(int x) {
+	public void moveX(float x) {
 		curX += x;
 	}
 
-	public void setY(int y) {
-		curX = y;
+	public void moveY(float y) {
+		curY += y;
 	}
 
 	public void render(GameContainer gc, Graphics g) throws SlickException {
-	try {
-		switch (action) {
-		case IDLE:
-			idleAnimation.draw(curX, curY);
-			break;
-		case WALKINGR:
-			walkingAnimation.draw(curX, curY);
-			break;
-		case WALKINGL:
-			walkingAnimation.draw(curX, curY);
-			break;
+		System.out.println("Current Action: " + action);
+		try {
+			switch (action) {
+			case IDLE:
+				idleAnimation.draw(curX, curY);
+				break;
+			case JUMPING:
+				idleAnimation.draw(curX, curY);
+				break;
+			case WALKINGR:
+				walkingAnimation.draw(curX, curY);
+				break;
+			case WALKINGL:
+				walkingLeftAnimation.draw(curX, curY);
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	} catch(Exception e) {
-		e.printStackTrace();
+		//g.draw(playerPoly);
 	}
+
+	public boolean entityCollisionWith() throws SlickException {
+		for (int i = 0; i < BlockMap.entities.size(); i++) {
+			Block entity1 = (Block) BlockMap.entities.get(i);
+			entity = entity1;
+			if (playerPoly.intersects(entity1.poly)) {
+				System.out.println("entity collide TRUE");
+				return true;
+			}
+		}
+		return false;
 	}
 }
